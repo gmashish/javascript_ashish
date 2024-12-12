c4ffbf39920e3c4018a88489e0a332b551bc8e79
Ashish Gharti Magar <gmashish202@gmail.com>
2024-12-12 07:24:50 +0000

    typeconversion

---
+++ typeconversion.js
@@ -0,0 +1,18 @@
+// let score = "33"
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber);
+//  let score = "45" we cannot redeclaration the let score value it show the error
+// let isLogedIn = 1
+// let booleanIsLogedIn = Boolean(isLogedIn)
+// console.log(booleanIsLogedIn);
+//  1=> true 0=>false   ashish=>true   Null=>false
+// let somenum = 77
+// let changenum = String(somenum)
+// console.log(changenum);
+// console.log(typeof(changenum));
+let somenum = "nepal"
+let changenum = Number(somenum)
+console.log(changenum);
+console.log(typeof(changenum));

e1ba25e59a112fcddc935ff7f28947814bf62fa3
Ashish Gharti Magar <gmashish202@gmail.com>
2024-12-14 17:00:46 +0000

    done

---
+++ variable.js
@@ -0,0 +1,25 @@
+// const name = "ashish";
+// const myRepo = 50;
+// console.log(`${name} and ${myRepo}`);
+// const gameName= new String ('ashishghartimagar')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__)
+// console.log(gameName.length);
+// console.log(gameName.toLocaleUpperCase());
+// console.log(gameName.charAt(5))
+// console.log(gameName.indexOf('h'))
+// const newString= gameName.substring(0,4)
+// console.log(newString);
+// const anotherString=gameName.slice(-16,5);
+// console.log(anotherString);
+
+// const newString= "   nepal  "
+// console.log(newString)
+// console.log(newString.trim())
+// const url = "htpps://ashish.com/ashish%20gm"
+// console.log(url.replace('%20','_'));
+// console.log(url.includes('ashish'));
+// console.log(url.includes('op'));
+
+const Op="i_love_nepal"
+console.log(Op.split('_'));
